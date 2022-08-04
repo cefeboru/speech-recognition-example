23d580b9e386754db7d9ee83e8acff2418c86a9b
Cesar Bonilla <cesar.bonilla@beon.studio>
2022-08-03 21:55:21 -0600

    Dynamic Mic icon on mouse up & down

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import './App.css';
-import MicOffIcon from '@mui/icons-material/MicOff';
-// import MicNoneIcon from '@mui/icons-material/MicNone';
-import IconButton from '@mui/material/IconButton';
+import "./App.css";
+
+import React from "react";
+import MicOffIcon from "@mui/icons-material/MicOff";
+import MicIcon from "@mui/icons-material/Mic";
+import IconButton from "@mui/material/IconButton";
 
 function App() {
+  const [isLestening, setIsListening] = React.useState(false);
+
+  const Icon = isLestening ? MicIcon : MicOffIcon;
+
+  const handleMouseDown = () => {
+    setIsListening(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsListening(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <IconButton className='mic-button' color='primary' aria-label="listen" size="large">
-          <MicOffIcon className='mic-icon'/>
+        <IconButton
+          className="mic-button"
+          color="primary"
+          aria-label="listen"
+          size="large"
+        >
+          <Icon
+            className="mic-icon"
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+          />
         </IconButton>
       </header>
     </div>
